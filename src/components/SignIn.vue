205bc2a9346fb50e1940911bf058ea8583c8c36a
ddmitiy <dudov.dm@gmail.com>
2018-03-23 23:08:09 +0300

    add post request on sign in(login)

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,34 +1,54 @@
 <template>
   <v-card>
-    <v-card-title
-    >
-      Вход
-    </v-card-title>
-    <v-text-field
-      label="Имя пользователя"
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      min="8"
-      :append-icon-cb="() => (e1 = !e1)"
-      :type="e1 ? 'password' : 'text'"
-      label="Пароль"
-    ></v-text-field>
-    <v-btn >
-      Отправить
-    </v-btn>
+    <v-form ref="form" lazy-validation>
+      <v-card-title
+      >
+        Вход
+      </v-card-title>
+      <v-text-field
+        v-model="name"
+        label="Имя пользователя"
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        min="8"
+        :append-icon-cb="() => (e1 = !e1)"
+        :type="e1 ? 'password' : 'text'"
+        label="Пароль"
+      ></v-text-field>
+      <v-btn
+        @click="submit"
+      >
+        Отправить
+      </v-btn>
+    </v-form>
   </v-card>
 </template>
 
 <script>
-    export default {
-      name: "sign-in",
-      data() {
-        return {
-          e1: true
+  import axios from 'axios'
+
+  export default {
+    name: "sign-in",
+    data:() => ({
+      e1: true,
+      name: '',
+      password: '',
+      methods: {
+        submit() {
+          if (this.$refs.form.validate()) {
+            axios.post('/api/auth/login', {
+              name: this.name,
+              password: this.password
+            })
+          }
+        },
+        clear() {
+          this.$refs.form.reset()
         }
       }
-    }
+    })
+  }
 </script>
 
 <style scoped>
