500cdadc01e9583a3a41b8eac7f2dee49c6e7705
obuhhh <obuhhh@yandex.ua>
2018-03-24 14:04:16 +0200

    Костяк для десигна

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,53 +1,49 @@
 <template>
   <v-layout>
     <v-flex xs12 id="bg">
-      <v-layout row>
-        <v-flex xs7 offset-xs1 pt-3>
-          <h1 style="float: left; padding-left: 50px">2 chairs</h1>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex xs6 offset-xs1 pt-5>
-          <h2 class="footer">
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-            Some text about our project
-          </h2>
-        </v-flex>
-        <v-flex xs1 offset-xs1>
-          <v-dialog v-model='loginDialog' attach='#app' max-width="500px">
-            <v-btn
-              class="footer"
-              slot="activator"
-            >
-              Sign In
-            </v-btn>
-            <SignIn/>
-          </v-dialog>
-        </v-flex>
-        <v-flex xs1 ml-5>
-          <v-dialog v-model='registerDialog' attach='#app' max-width="500px">
-            <v-btn
-              class="footer"
-              slot="activator"
-            >
-              Sign Up
-            </v-btn>
-            <SignUp/>
-          </v-dialog>
-        </v-flex>
-      </v-layout>
+      <div id="bg-gradient">
+        <v-container fill-height id="grid-container">
+          <v-layout class="">
+            <v-flex class="px-3" xs3 offset-xs1>
+              <div style="background-color: rgba(255, 255, 255, 0.5)">
+                sdf
+              </div>
+            </v-flex>
+            <v-flex class="px-3" xs2>
+              <div class="line-in-middle" style="background-color: rgba(255, 255, 255, 0.5)">
+              </div>
+            </v-flex>
+            <v-flex class="px-3" xs5>
+              <div style="background-color: rgba(255, 255, 255, 0.5)">
+                sdf
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </div>
     </v-flex>
   </v-layout>
 </template>
+
+
+<!--<v-dialog v-model='loginDialog' attach='#app' max-width="500px">
+  <v-btn
+    class="footer"
+    slot="activator"
+  >
+    Sign In
+  </v-btn>
+  <SignIn/>
+</v-dialog>
+<v-dialog v-model='registerDialog' attach='#app' max-width="500px">
+  <v-btn
+    class="footer"
+    slot="activator"
+  >
+    Sign Up
+  </v-btn>
+  <SignUp/>
+</v-dialog>-->
 
 <script>
   import SignIn from './SignIn'
@@ -55,11 +51,11 @@
 
   export default {
     name: "home",
-    components:{
+    components: {
       SignIn,
       SignUp
     },
-    data:() => ({
+    data: () => ({
       loginDialog: '',
       registerDialog: ''
     })
@@ -67,15 +63,39 @@
 </script>
 
 <style scoped>
-  #bg{
+
+  .line-in-middle {
+    height:200px;
+    background: linear-gradient(to right,
+    transparent 0%,
+    transparent calc(50% - 0.81px),
+    white calc(50% - 0.8px),
+    white calc(50% + 0.8px),
+    transparent calc(50% + 0.81px),
+    transparent 100%);
+  }
+
+  #grid-container {
+    width: 1110px;
+  }
+
+  #bg-gradient {
+    background: linear-gradient(118.07deg, #7654FF 0%, #A55AE0 29.98%, #FF855E 100%);
+    opacity: 0.8;
+    height: 100%;
+  }
+
+  #bg {
     background-image: url('/static/bg_2chair.jpg');
     background-size: 100%;
   }
-  .footer{
+
+  .footer {
     margin-top: 300px;
     margin-bottom: 100px;
   }
-  .dialog{
+
+  .dialog {
     background-color: white;
   }
 </style>
